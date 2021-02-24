c7a0a1997a424f9968094960b8706f10cd09a7d2
Jamison Moody <mood2jam@gmail.com>
2021-02-24 16:50:21 -0700

    Changed the Square Movement Class

--- mechanics/Entity.py
+++ mechanics/Entity.py
@@ -1,31 +1,41 @@
 import uuid
 
-def square_dict(self):
-    sq = {}
-    # Key[level]            (Min, Max)
-    sq[1] = (1, 1)  # num_grids
-    sq[2] = (1, 1)  # square_types
-    # Should record square type we are on
-    sq[4] = (1, 6)  # piece_types
-    # Should record piece type we are on
-    sq[5] = (1, 3)  # num_action_types
-    sq[6] = (1, 4)  # selected_action_type. Needs to be masked out
-    sq[7] = (1, 2)  # num_patterns
-    # Should record which pattern we are looking at
-    sq[8] = (1, 3)  # pattern_length
-    sq[9] = (1, 9)  # pattern_symbols
-    return sq
+interpret_level = {
+      0: "mechanic_num",
+      1: "num_groups",
+      2: "selected_group",
+      3: "selected_parent_entity", # Defaults to 0
+      4: "num_child_entities",
+      5: "selected_child_entity",
+      6: "num_action_types",
+      7: "selected_action_type",
+      8: "num_patterns",
+      9: "selected_pattern",
+      10: "pattern_length",
+      11: "pattern_symbol"
+    }
 
 class Entity():
-    def __init__(self, entity_type, sub_type, parent, id=None, p=None):
+    def __init__(self, entity_names=None, parent_names=None, parents_to_actions=None, actions_to_patterns=None, id=None, p=None):
+        """
+        :param entity_names: List of names for of each piece type that the entity belongs to. Each name includes a generic
+        name (from the mechanic) and a subtype.
+        :param parent_names: Names of the parents of this entity. This specifies which entity types it can move across.
+        Each name includes a generic name (from the mechanic) and a subtype.
+        :param parents_to_actions: A dictionary mapping a parent name to a list of action types (strings from mechanic)
+        :param actions_to_patterns: A dictionary mapping an action type (string) to an action pattern (a sequence of entity
+        group actions)
+        :param id: An id that can be used to identify this distinct entity in the future
+        :param p: A dictionary of parameters to fill in any more information
+        """
         self.p = p
-        self.entity_type = entity_type # string e.x. Square_Grid
-        self.parents = [parent] # Types that the entity can move across
-        self.entity_type_list = [entity_type + '_' + str(sub_type)]
+        self.entity_names = set() if entity_names is None else entity_names
+        self.parent_names = set() if parent_names is None else parent_names
+        self.parents_to_actions = dict() if parents_to_actions is None else parents_to_actions
+        self.actions_to_patterns = dict() if actions_to_patterns is None else actions_to_patterns
         if id is None:
             self.id = uuid.uuid1()
         else:
             self.id = id
-        self.my_store_types = []
-        self.my_storage_location = 0
+        self.storage_location = None
         self.my_stored_ids = []
--- mechanics/Square_Grid_Movement.py
+++ mechanics/Square_Grid_Movement.py
@@ -1,17 +1,105 @@
 import numpy as np
 from mechanics.Entity import Entity
 from mechanics.Mechanic import Mechanic
+from rl_code.Game import interpret_level
 
 class Square_Grid_Movement_Class(Mechanic):
-    def __init__(self, p):
-        self.p = p
-        self.mechanic_type = p['mechanic_type']
-        self.grid_height = p['grid_height']
-        self.grid_width = p['grid_width']
+    def __init__(self, p=None):
+        super().__init__(p)
+        if p is None:
+            self.p = {"grid_height" : 5,
+                      "grid_width" : 5,
+                      "reflect_patterns": True}
+        self.mechanic_type = "Square Grid Movement"
+        self.grid_height = self.p['grid_height']
+        self.grid_width = self.p['grid_width']
+        self.parent_entity_names = {1: "square", 2: "reserve"}
+        self.child_entity_name = "square_movement_piece"
+        self.action_types = {"default": ["remove_captured_piece"],
+                             1: "drag_to_capture",
+                             2: "hop_to_capture",
+                             3: "hop_over_capture",
+                             4: "drag",
+                             5: "hop",
+                             6: "replace_captured_piece"}
+        self.reflect_patterns = self.p["reflect_patterns"]
+
+        # These pattern symbols can be used for all the actions
+        self.pattern_symbols1 = {
+            1: "NW", 2: "N", 3: "NE", 4: "W", 5: "E", 6: "SW", 7: "S", 8: "SE", 9: "*"
+        }
+        self.pattern_symbols2 = {
+            i: "transfer" for i in range(9)
+        }
+        self.all_pattern_symbols = {"remove_captured_piece" : self.pattern_symbols2,
+                             "drag_to_capture": self.pattern_symbols1,
+                             "hop_to_capture": self.pattern_symbols1,
+                             "hop_over_capture": self.pattern_symbols1,
+                             "drag": self.pattern_symbols1,
+                             "hop": self.pattern_symbols1,
+                             "replace_captured_piece": self.pattern_symbols2}
 
 
-    def create_entity(self):
-        entities = []
-        for i in range(self.width * self.grid_height):
-            entities.append( Entity( self.p, self.mechanic_type ) )
-        return entities
+    def square_dict(self):
+        sq = {}
+        # Key[level]            (Min, Max)
+        sq["num_groups"] = (1, max([x for x in self.parent_entity_names.keys() if type(x) is int]))  # num_group (how many of that mechanic there is)
+        # Should record which group we are on
+        sq["num_child_entities"] = (1, 6)  # num child entity types
+        # Should record child entity type we are on
+        sq["num_action_types"] = (1, max([x for x in self.action_types.keys() if type(x) is int]))  # num_action_types
+        sq["num_patterns"] = (1, 4)  # num_patterns
+        # Should record which pattern we are looking at
+        sq["pattern_length"] = (1, 3)  # pattern_length
+        sq["pattern_symbol"] = (1, max([x for x in self.pattern_symbols.keys() if type(x) is int]))  # pattern_symbol
+        sq["num_parent_entity_types"] = 2
+        return sq
+
+    interpret_level = {
+        0: "mechanic_num",
+        1: "num_groups",
+        2: "selected_group",
+        3: "selected_parent_entity",  # Defaults to 0
+        4: "num_child_entities",
+        5: "selected_child_entity",
+        6: "num_action_types",
+        7: "selected_action_type",
+        8: "num_patterns",
+        9: "selected_pattern",
+        10: "pattern_length",
+        11: "pattern_symbol"
+    }
+
+    def add_to_entity(self, entity, tree_trajectory):
+        [1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 3, 7]
+        detailed_trajectory = {interpret_level[i]: val for i, val in enumerate(tree_trajectory)}
+        if detailed_trajectory["selected_parent_entity"] == 0:
+            # Child entity
+            entity.entity_names.add(self.child_entity_name + detailed_trajectory["selected_child_entity"])
+
+            # Add parent name
+            parent_name = self.parent_entity_names[detailed_trajectory["selected_parent_entity"]] + detailed_trajectory["selected_group"]
+            entity.parent_names.add(parent_name)
+
+            # Set up action type
+            action_type = self.action_types[detailed_trajectory["selected_action_type"]]
+            try:
+                entity.parent_to_actions[parent_name].add(action_type)
+            except KeyError:
+                entity.parent_to_actions[parent_name] = {action_type}
+
+            # Add to pattern
+            pattern_symbol = self.all_pattern_symbols[action_type][detailed_trajectory["pattern_symbol"]]
+            try:
+                self.actions_to_patterns[action_type]
+            except KeyError:
+                self.actions_to_patterns
+
+            self.parents_to_actions = set() if parents_to_actions is None else parents_to_actions
+            self.actions_to_patterns = set() if actions_to_patterns is None else actions_to_patterns
+
+        else:
+            # Parent entity
+
+
+        return entity
