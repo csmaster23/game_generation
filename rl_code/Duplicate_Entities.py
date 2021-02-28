b8a42719a2a813cfd0cc87370915c442fd8f6a1b
Tanner Norton <tanneranorton23@gmail.com>
2021-02-27 17:34:12 -0700

    dup updates

--- rl_code/Duplicate_Entities.py
+++ rl_code/Duplicate_Entities.py
@@ -69,27 +69,7 @@ class Duplicate_Entities_Model():
                     min_max[mm][0] = (min_max[mm][0][0], min_max[mm][0][1] - chosen_num)
         return min_max
 
-        # indices = self.get_key(embeddings[i], comb_map)
-        # if 'str' in str(type(indices)):       # original entity, i.e. non combined entity
-        #     min_max[i] = (min_max[i][0], min_max[i][1] - chosen_num)# subtract the chosen num from the max of the corresponding min_max entry for embed row i
-        # else:                                   # combined entity
-        #     ind_one = indices[0]
-        #     ind_two = indices[1]
-        #     one_indices = self.get_key(embeddings[ind_one], comb_map)
-        #     if 'tuple' in str(type(one_indices)): # means you've hit the base
-        #         min_max[one_indices[0]] = (min_max[one_indices[0]][0], min_max[one_indices[0]][1] - chosen_num)
-        #         min_max[one_indices[1]] = (min_max[one_indices[1]][0], min_max[one_indices[1]][1] - chosen_num)
-        #
-        #     two_indices = self.get_key(embeddings[ind_two], comb_map)
-        #     if 'tuple' in str(type(two_indices)): # means you've hit the base
-        #         min_max[two_indices[0]] = (min_max[two_indices[0]][0], min_max[two_indices[0]][1] - chosen_num)
-        #         min_max[two_indices[1]] = (min_max[two_indices[1]][0], min_max[two_indices[1]][1] - chosen_num)
-        #
-        #     min_max[ind_one] = (min_max[ind_one][0], min_max[ind_one][1] - chosen_num)
-        #     min_max[ind_two] = (min_max[ind_two][0], min_max[ind_two][1] - chosen_num)
-        #     min_max[i] = (min_max[i][0], min_max[i][1] - chosen_num)
-        #
-        # return min_max
+
 
     def get_all_min_max_dict(self, embeddings, trajectories, comb_map):
         min_max = {} # dictionary of embedding number as key and value that is list with two entries, first is tuple of min max and second is list of original entity combiners
@@ -119,72 +99,6 @@ class Duplicate_Entities_Model():
 
         return min_max
 
-    def get_parent_min_max_dict(self, embeddings, trajectories, comb_map):
-        min_max = {} # dictionary of embedding number as key and value that is list with two entries, first is tuple of min max and second is list of original entity combiners
-        for i, emb in enumerate(embeddings):
-            if i < len(trajectories):                                                   # these are embeddings we have trajectories for
-                if trajectories[i][0] == self.mechanic_types['Square-Grid Movement']:
-                    min_max[i] = [(5, 8), []] # the empty list denotes that nothing goes to combine an original entity
-                elif trajectories[i][0] == self.mechanic_types['Betting']:
-                    min_max[i] = [(2, 4), []]
-                else:
-                    min_max[i] = [(1, 1), []] # idk what this would be but it's here
-            else:                                                                       # embeddings that we just created through combinations that we have no trajectories for
-                indices = self.get_key( emb, comb_map )                                 # will be in embedding order
-                first_min_max = min_max[indices[0]][0][1]       # [0][1] to get tuple of min max and single out max
-                second_min_max = min_max[indices[1]][0][1]      # [0][1] to get tuple of min max and single out max
-                first_origins_lst = min_max[indices[0]][1]      # gets list of combined origins from indices
-                second_origins_lst = min_max[indices[1]][1]     # gets list of combined origins from indices
-                min_max[i] = [( 1, min([first_min_max, second_min_max]) ), first_origins_lst + second_origins_lst + [indices[0], indices[1]]]
-
-        return min_max
-# mechanic_types = {
-#     "Square-Grid Movement": 1,
-#     "Betting": 2,
-# }
-    def get_child_min_max_dict(self, embeddings, trajectories, comb_map):
-        min_max = {}  # dictionary of embedding number as key and value that is list with two entries, first is tuple of min max and second is list of original entity combiners
-        for i, emb in enumerate(embeddings):
-            if i < len(trajectories):  # these are embeddings we have trajectories for
-                if trajectories[i][0][0] == self.mechanic_types['Square-Grid Movement']:
-                    min_max[i] = [(3, 6), []]  # the empty list denotes that nothing goes to combine an original entity
-                elif trajectories[i][0][0] == self.mechanic_types['Betting']:
-                    min_max[i] = [(2, 4), []]
-                else:
-                    min_max[i] = [(1, 1), []]  # idk what this would be but it's here
-            else:  # embeddings that we just created through combinations that we have no trajectories for
-                indices = self.get_key(emb, comb_map)  # will be in embedding order
-                first_min_max = min_max[indices[0]][0][1]  # [0][1] to get tuple of min max and single out max
-                second_min_max = min_max[indices[1]][0][1]  # [0][1] to get tuple of min max and single out max
-                first_origins_lst = min_max[indices[0]][1]  # gets list of combined origins from indices
-                second_origins_lst = min_max[indices[1]][1]  # gets list of combined origins from indices
-                min_max[i] = [(1, min([first_min_max, second_min_max])),
-                              first_origins_lst + second_origins_lst + [indices[0], indices[1]]]
-        return min_max
-        # return min_max
-        # min_max = {}
-        # for i, traj in enumerate(trajectories):
-        #     if traj[0][0] == self.mechanic_types['Square-Grid Movement']:
-        #         min_max[i] = (3, 6)
-        #     elif traj[0][0] == self.mechanic_types['Betting']:
-        #         min_max[i] = (2, 4)
-        #     else:
-        #         min_max[i] = (1, 1)
-        # return min_max
-# interpret_level = {
-#     0: "mechanic_num",
-#     1: "num_groups",
-#     2: "selected_group",
-#     3: "selected_parent_entity",  # Defaults to 0
-#     4: "num_child_entities",
-#     5: "selected_child_entity",
-#     6: "num_action_types",
-#     7: "selected_action_type",
-#     8: "num_patterns",
-#     9: "selected_pattern",
-#     10: "pattern_length",
-#     11: "pattern_symbol"
-# }
 
     def get_mask(self, N ): # N in comes in as a tuple of min, max
         full_zeros = torch.zeros((self.embedding_size,))
@@ -199,3 +113,96 @@ class Duplicate_Entities_Model():
             if torch.all(torch.eq(val, value)):
                 return key
         return "key doesn't exist"
+
+    # def get_parent_min_max_dict(self, embeddings, trajectories, comb_map):
+    #     min_max = {} # dictionary of embedding number as key and value that is list with two entries, first is tuple of min max and second is list of original entity combiners
+    #     for i, emb in enumerate(embeddings):
+    #         if i < len(trajectories):                                                   # these are embeddings we have trajectories for
+    #             if trajectories[i][0] == self.mechanic_types['Square-Grid Movement']:
+    #                 min_max[i] = [(5, 8), []] # the empty list denotes that nothing goes to combine an original entity
+    #             elif trajectories[i][0] == self.mechanic_types['Betting']:
+    #                 min_max[i] = [(2, 4), []]
+    #             else:
+    #                 min_max[i] = [(1, 1), []] # idk what this would be but it's here
+    #         else:                                                                       # embeddings that we just created through combinations that we have no trajectories for
+    #             indices = self.get_key( emb, comb_map )                                 # will be in embedding order
+    #             first_min_max = min_max[indices[0]][0][1]       # [0][1] to get tuple of min max and single out max
+    #             second_min_max = min_max[indices[1]][0][1]      # [0][1] to get tuple of min max and single out max
+    #             first_origins_lst = min_max[indices[0]][1]      # gets list of combined origins from indices
+    #             second_origins_lst = min_max[indices[1]][1]     # gets list of combined origins from indices
+    #             min_max[i] = [( 1, min([first_min_max, second_min_max]) ), first_origins_lst + second_origins_lst + [indices[0], indices[1]]]
+    #
+    #     return min_max
+    #
+    # def get_child_min_max_dict(self, embeddings, trajectories, comb_map):
+    #     min_max = {}  # dictionary of embedding number as key and value that is list with two entries, first is tuple of min max and second is list of original entity combiners
+    #     for i, emb in enumerate(embeddings):
+    #         if i < len(trajectories):  # these are embeddings we have trajectories for
+    #             if trajectories[i][0][0] == self.mechanic_types['Square-Grid Movement']:
+    #                 min_max[i] = [(3, 6), []]  # the empty list denotes that nothing goes to combine an original entity
+    #             elif trajectories[i][0][0] == self.mechanic_types['Betting']:
+    #                 min_max[i] = [(2, 4), []]
+    #             else:
+    #                 min_max[i] = [(1, 1), []]  # idk what this would be but it's here
+    #         else:  # embeddings that we just created through combinations that we have no trajectories for
+    #             indices = self.get_key(emb, comb_map)  # will be in embedding order
+    #             first_min_max = min_max[indices[0]][0][1]  # [0][1] to get tuple of min max and single out max
+    #             second_min_max = min_max[indices[1]][0][1]  # [0][1] to get tuple of min max and single out max
+    #             first_origins_lst = min_max[indices[0]][1]  # gets list of combined origins from indices
+    #             second_origins_lst = min_max[indices[1]][1]  # gets list of combined origins from indices
+    #             min_max[i] = [(1, min([first_min_max, second_min_max])),
+    #                           first_origins_lst + second_origins_lst + [indices[0], indices[1]]]
+    #     return min_max
+
+    # mechanic_types = {
+    #     "Square-Grid Movement": 1,
+    #     "Betting": 2,
+    # }
+
+
+    # indices = self.get_key(embeddings[i], comb_map)
+    # if 'str' in str(type(indices)):       # original entity, i.e. non combined entity
+    #     min_max[i] = (min_max[i][0], min_max[i][1] - chosen_num)# subtract the chosen num from the max of the corresponding min_max entry for embed row i
+    # else:                                   # combined entity
+    #     ind_one = indices[0]
+    #     ind_two = indices[1]
+    #     one_indices = self.get_key(embeddings[ind_one], comb_map)
+    #     if 'tuple' in str(type(one_indices)): # means you've hit the base
+    #         min_max[one_indices[0]] = (min_max[one_indices[0]][0], min_max[one_indices[0]][1] - chosen_num)
+    #         min_max[one_indices[1]] = (min_max[one_indices[1]][0], min_max[one_indices[1]][1] - chosen_num)
+    #
+    #     two_indices = self.get_key(embeddings[ind_two], comb_map)
+    #     if 'tuple' in str(type(two_indices)): # means you've hit the base
+    #         min_max[two_indices[0]] = (min_max[two_indices[0]][0], min_max[two_indices[0]][1] - chosen_num)
+    #         min_max[two_indices[1]] = (min_max[two_indices[1]][0], min_max[two_indices[1]][1] - chosen_num)
+    #
+    #     min_max[ind_one] = (min_max[ind_one][0], min_max[ind_one][1] - chosen_num)
+    #     min_max[ind_two] = (min_max[ind_two][0], min_max[ind_two][1] - chosen_num)
+    #     min_max[i] = (min_max[i][0], min_max[i][1] - chosen_num)
+    #
+    # return min_max
+
+    # return min_max
+    # min_max = {}
+    # for i, traj in enumerate(trajectories):
+    #     if traj[0][0] == self.mechanic_types['Square-Grid Movement']:
+    #         min_max[i] = (3, 6)
+    #     elif traj[0][0] == self.mechanic_types['Betting']:
+    #         min_max[i] = (2, 4)
+    #     else:
+    #         min_max[i] = (1, 1)
+    # return min_max
+    # interpret_level = {
+    #     0: "mechanic_num",
+    #     1: "num_groups",
+    #     2: "selected_group",
+    #     3: "selected_parent_entity",  # Defaults to 0
+    #     4: "num_child_entities",
+    #     5: "selected_child_entity",
+    #     6: "num_action_types",
+    #     7: "selected_action_type",
+    #     8: "num_patterns",
+    #     9: "selected_pattern",
+    #     10: "pattern_length",
+    #     11: "pattern_symbol"
+    # }
